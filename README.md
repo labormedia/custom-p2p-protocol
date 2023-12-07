eafe0b6c7c8baa0290a226785ddd90066e1620f1
Diego Correa Tristain <algoritmia@labormedia.cl>
2023-12-07 18:38:22 +0000

    Adds networks to the magic_bytes registry.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # custom-p2p-protocol
-This is an example implementation of a p2p protocol based on https://developer.bitcoin.org/reference/p2p_networking.html#p2p-network .
+This is an example implementation of a p2p protocol based on https://en.bitcoin.it/wiki/Protocol_documentation .
 
 ## Run in debug mode:
 ```
--- src/magic_bytes.rs
+++ src/magic_bytes.rs
@@ -2,30 +2,48 @@ use crate::EndianWrite;
 
 pub enum Network {
     Mainnet,
-    Testnet,
+    Testnet3,
+    Regtest,
+    Signet,
+    Namecoin,
+
 }
 
-const MAINNET_LE: [u8;4] = [0xf9, 0xbe, 0xb4, 0xd9];
-const TESTNET_LE: [u8;4] = [0x0b, 0x11, 0x09, 0x07];
+const MAINNET_LE: [u8;4] = [0xd9, 0xb4, 0xbe, 0xf9]; // as documented in https://en.bitcoin.it/wiki/Protocol_documentation#Common_structures
+const TESTNET3_LE: [u8;4] = [0x07, 0x09, 0x11, 0x0b]; // as documented in https://en.bitcoin.it/wiki/Protocol_documentation#Common_structures
+const REGTEST_LE: [u8; 4] = [0xda, 0xb5, 0xbf, 0xfa];
+const SIGNET_LE: [u8; 4] = [0x40, 0xcf, 0x03, 0x0a];
+const NAMECOIN_LE: [u8;4] = [0xfe, 0xb4, 0xbe, 0xf9];
 
 impl EndianWrite for Network {
     type Output = [u8;4];
     fn to_le_bytes(&self) -> Self::Output {
+        let mut buf = self.to_be_bytes().clone();
+        buf.reverse();
+        buf
+    }
+    fn to_be_bytes(&self) -> Self::Output {
         match self {
             Network::Mainnet => {
                 MAINNET_LE
             },
-            Network::Testnet => {
-                TESTNET_LE
+            Network::Testnet3 => {
+                TESTNET3_LE
+            },
+            Network::Regtest => {
+                REGTEST_LE
+            },
+            Network::Signet => {
+                SIGNET_LE
+            },
+            Network::Namecoin => {
+                NAMECOIN_LE
             }
         }
     }
-    fn to_be_bytes(&self) -> Self::Output {
-        let mut buf = self.to_le_bytes().clone();
-        buf.reverse();
-        buf
-    }
 }
+
+// const ALL_NETWORKS_LIST = [ Network::Mainnet, Network::Testnet3, Network::Regtest, Network::Signet, Network::Namecoin];
 
 #[test]
 #[should_panic]
