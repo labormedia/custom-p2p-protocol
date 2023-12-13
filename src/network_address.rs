bd0eff86a11f990bd57c88902251c18aef3b07df
Diego Correa Tristain <algoritmia@labormedia.cl>
2023-12-13 02:15:46 +0000

    Refactor impl EndianWrite for IP in network_address.

--- src/network_address.rs
+++ src/network_address.rs
@@ -16,22 +16,18 @@ impl EndianWrite for IP {
     type Output = [u8; NETWORK_IPvXX];
     fn to_le_bytes(&self) -> Self::Output {
         match self {
-            Self::V4(network_address) => {
-                *network_address
+            Self::V4(network_address) | Self::V6(network_address) => {
+                let mut buf: Self::Output = *network_address;
+                buf.reverse();
+                buf
             },
-            Self::V6(network_address) => {
-                *network_address
-            }
         }
     }
     fn to_be_bytes(&self) -> Self::Output {
         match self {
-            Self::V4(network_address) => {
+            Self::V4(network_address) | Self::V6(network_address) => {
                 *network_address
             },
-            Self::V6(network_address) => {
-                *network_address
-            }
         }
     }
 }
@@ -39,5 +35,5 @@ impl EndianWrite for IP {
 // Defines use of network address (they differ)
 pub enum NetworkAddress { 
     General([u8;NETWORK_TIME], [u8;NETWORK_SERVICES], [u8;NETWORK_IPvXX], [u8;NETWORK_PORT]),
-    Version(),
+    Version([u8;NETWORK_SERVICES], [u8;NETWORK_IPvXX], [u8;NETWORK_PORT]),
 }
